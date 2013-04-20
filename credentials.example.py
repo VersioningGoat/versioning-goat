6d3a318c591ab49dc5ccb5f6b1eb903294c9e5d7
Ben Regenspan <ben@sohotechlabs.com>
2013-04-20 15:45:41 -0400

    Add archive extraction, github repo creation (untested)

---
+++ credentials.example.py
@@ -0,0 +1,14 @@
+# GitHub app credentials, obtain by registering an app at:
+#   https://github.com/settings/applications/
+GITHUB_ID = ''
+GITHUB_SECRET = ''
+
+# User access token, obtain by following the steps described at:
+#   https://github.com/michaelliao/githubpy#authentication
+GITHUB_TOKEN = ''
+
+
+GITHUB_USERNAME = ''
+
+#TODO: handle organization, this will be ignored currently
+GITHUB_ORGANIZATION = False
--- versioning-goat/app.py
+++ versioning-goat/app.py
@@ -1,9 +1,26 @@
+import os
+import sys
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(os.path.join(PROJECT_ROOT, '..', './lib'))
+
+from archive import Archive
 from flask import Flask, request
-import magic
-import re
+from github import GitHub
 import requests
+from StringIO import StringIO
+
+from credentials import GITHUB_TOKEN, GITHUB_USERNAME
 
 """
+POSSIBLE INPUTS:
+
+* URL of static package.  We just assume this always refers to the latest version and pull
+    in periodically.  e.g. http://opensource.gsfc.nasa.gov/projects/DQSS/dqss_64.tar
+* Sourceforge file archive.  We get pinged re: when to update via their email notifications
+* Sourceforge repo (probably ignore this use case, NASA projects don't seem to use)
+* Third-party SVN
+
 Format for snapshot request URL:
 https://sourceforge.net/p/djangoajaxcl/code/1/tarball
 
@@ -17,6 +34,8 @@ wget http://sourceforge.net/projects/phppgadmin/files/latest/download
 
 
 SOURCEFORGE_URL_FORMAT = "http://sourceforge.net/projects/%s/files/latest/download"
+REPOS_FOLDER = os.path.join(PROJECT_ROOT, '/repos')
+
 
 app = Flask(__name__)
 
@@ -26,15 +45,41 @@ projects = {
          }
 }
 
+def setup_repos():
+    """
+    """
 
-def retrieve_archive(url):
+    github = GitHub(access_token=GITHUB_TOKEN)
+
+    for name, data in projects.iteritems():
+        github.users(GITHUB_USERNAME).repos.post(
+            name='nasa-%s' % (data['short_name']),
+            description='Mirrored repository')
+
+
+def retrieve_file(url):
+    """ Retrieve a remote file and return a file-like object for it
+
+        In: `url`
+        Out: file-like object
+    """
     response = requests.get(url)
-    return response.content
+
+    filelike = StringIO(response.content)
+
+    # augment with filename so Archive and other packages know how to handle
+    filelike.name = response.url.split('/')[-1]
+
+    # TODO: use content-type header to generate filename
+    #   if we don't get good name from server.
+    # content_type = response.headers['content-type']
+
+    return filelike
 
 
 @app.route("/ping", methods=['POST'])
 def process_ping():
-    """ We've just been pinged that a repository has been updated.
+    """ Process an inbound ping re: a repository being updated.
 
     Assume ping matches format from Sendgrid:
     http://sendgrid.com/docs/API_Reference/Webhooks/parse.html
@@ -43,7 +88,10 @@ def process_ping():
     for projectname, data in projects.iteritems():
         if projectname in request.form['subject']:
             download_url = SOURCEFORGE_URL_FORMAT % (data['short_name'])
-            response = retrieve_archive(download_url)
+            response = retrieve_file(download_url)
+
+            # extract to current dir
+            Archive(response).extract(REPOS_FOLDER)
             # TODO: extract response archive, add new remote and Github repo
             #    if needed, push
             break
