26cf528155c0cb41d13e93d772295e78a68c83fc
Matias Anaya <matiasanaya@gmail.com>
2013-04-20 22:29:46 -0400

    Better comments

--- versioning-goat/std_readme.py
+++ versioning-goat/std_readme.py
@@ -2,8 +2,9 @@ import glob
 
 
 def write_std_readme(sync_method, repo_url, etag):
-    # Check if repo has readMe already
     header = "<hr>\n<img src='http://localhost:5000/status?repo_url=%s&etag=%s&sync_method=%s'> *this repository is automatically kept up to date by [the versioning goat](https://github.com/versioninggoat/versioning-goat).\n\nIt is officially maintained [here](#).\n<hr>\n" % (repo_url, etag, sync_method)
+    # Either inject header or create full-blown readMe file.
+    # TODO: add path to search for
     if len(glob.glob('README*')) > 0:
         filename = glob.glob('README*')[0]
     else:
@@ -18,7 +19,6 @@ def write_std_readme(sync_method, repo_url, etag):
         print 'README not found'
     modified = open(filename, 'w')
     modified.write(header + data)
-    # Either inject header or create full-blown readMe file.
 
 # if __name__ == "__main__":
 #     from sys import argv
