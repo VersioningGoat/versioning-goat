d58b6b35a113eadc45c31e33c07be32311d601fb
Matias Anaya <matiasanaya@gmail.com>
2013-04-21 09:25:04 -0400

    Adds proper goat assets

--- versioning-goat/indicator.py
+++ versioning-goat/indicator.py
@@ -6,18 +6,18 @@ import requests
 def get_image(request):
     # Checks for repos with push enables (aka SourceForge)
     if request.args.get('sync_method') == 'push':
-        filename = '../assets/images/ok.png'
+        filename = '../assets/images/goat_ok.png'
         return send_file(filename, mimetype='image/png')
     # Checks for goat-loving static files
     check = up_to_date(request.args.get('repo_url'), request.args.get('etag'))
     if check is True:
-        filename = '../assets/images/ok.png'
+        filename = '../assets/images/goat_ok.png'
     elif check is False:
-        filename = '../assets/images/not_ok.png'
+        filename = '../assets/images/goat_work.png'
         # TODO: Trigger Goat Worker
         # pass it request.args.get('name')
     else:
-        filename = '../assets/images/bad.png'
+        filename = '../assets/images/goat_error.png'
     return send_file(filename, mimetype='image/png')
 
 
